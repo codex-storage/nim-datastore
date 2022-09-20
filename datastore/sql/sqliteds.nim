fc842663bb253ba12243ad80031e34070a911bef
Dmitriy Ryajov <dryajov@gmail.com>
2022-09-19 22:40:01 -0600

    implement close method for sqlite

--- datastore/sql/sqliteds.nim
+++ datastore/sql/sqliteds.nim
@@ -68,6 +68,10 @@ method get*(self: SQLiteDatastore, key: Key): Future[?!seq[byte]] {.async.} =
 method put*(self: SQLiteDatastore, key: Key, data: seq[byte]): Future[?!void] {.async.} =
   return self.db.putStmt.exec((key.id, @data, timestamp()))
 
+method close*(self: SQLiteDatastore): Future[?!void] {.async.} =
+  self.db.close()
+  return success()
+
 # iterator query*(
 #   self: SQLiteDatastore,
 #   query: Query): Future[QueryResponse] =
