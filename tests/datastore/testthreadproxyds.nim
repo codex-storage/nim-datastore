6c86d2b10e6726bb0fb814e6e073bda556343554
Dmitriy Ryajov <dryajov@gmail.com>
2023-09-13 14:43:25 -0600

    make sure all tests pass

--- tests/datastore/testthreadproxyds.nim
+++ tests/datastore/testthreadproxyds.nim
@@ -1,6 +1,7 @@
 import std/options
 import std/sequtils
 import std/os
+import std/cpuinfo
 import std/algorithm
 
 import pkg/asynctest
@@ -9,55 +10,16 @@ import pkg/stew/results
 import pkg/stew/byteutils
 import pkg/taskpools
 
-import pkg/datastore/memoryds
+import pkg/datastore/sql
 import pkg/datastore/threads/threadproxyds
 
 import ./dscommontests
-# import ./querycommontests
+import ./querycommontests
 
-import pretty
-
-
-# suite "Test Basic ThreadProxyDatastore":
-#   var
-#     sds: ThreadDatastore
-#     mem: MemoryDatastore
-#     key1: Key
-#     data: seq[byte]
-#     taskPool: Taskpool
-
-#   setupAll:
-#     mem = MemoryDatastore.new()
-#     taskPool = TaskPool.new(3)
-#     sds = ThreadDatastore.new(mem, taskPool).expect("should work")
-#     key1 = Key.init("/a").tryGet
-#     data = "value for 1".toBytes()
-
-#   test "check put":
-#     echo "\n\n=== put ==="
-#     let res1 = await sds.put(key1, data)
-#     print "res1: ", res1
-
-#   test "check get":
-#     echo "\n\n=== get ==="
-#     let res2 = await sds.get(key1)
-#     check res2.get() == data
-#     var val = ""
-#     for c in res2.get():
-#       val &= char(c)
-
-#     print "get res2: ", $val
-
-#     # echo "\n\n=== put cancel ==="
-#     # # let res1 = await sds.put(key1, "value for 1".toBytes())
-#     # let res3 = sds.put(key1, "value for 1".toBytes())
-#     # res3.cancel()
-#     # # print "res3: ", res3
-
-suite "Test Basic ThreadProxyDatastore":
+suite "Test Basic ThreadDatastore":
 
   var
-    memStore: MemoryDatastore
+    memStore: Datastore
     ds: ThreadDatastore
     key = Key.init("/a/b").tryGet()
     bytes = "some bytes".toBytes
@@ -65,23 +27,29 @@ suite "Test Basic ThreadProxyDatastore":
     taskPool: Taskpool
 
   setupAll:
-    memStore = MemoryDatastore.new()
-    taskPool = Taskpool.new(2)
+    memStore = SQLiteDatastore.new(Memory).tryGet()
+    taskPool = Taskpool.new(countProcessors() * 2)
     ds = ThreadDatastore.new(memStore, taskPool).tryGet()
 
   teardownAll:
-    (await memStore.close()).get()
+    (await ds.close()).tryGet()
+    taskPool.shutdown()
 
   basicStoreTests(ds, key, bytes, otherBytes)
 
-# suite "Test Query":
-#   var
-#     mem: MemoryDatastore
-#     sds: ThreadProxyDatastore
+suite "Test Query ThreadDatastore":
+  var
+    mem: Datastore
+    ds: ThreadDatastore
+    taskPool: Taskpool
 
-#   setup:
-#     mem = MemoryDatastore.new()
-#     sds = newThreadProxyDatastore(mem).expect("should work")
+  setup:
+    taskPool = Taskpool.new(countProcessors() * 2)
+    mem = SQLiteDatastore.new(Memory).tryGet()
+    ds = ThreadDatastore.new(mem, taskPool).tryGet()
 
-#   queryTests(sds, false)
+  teardown:
+    (await ds.close()).tryGet()
+    taskPool.shutdown()
 
+  queryTests(ds, false)
